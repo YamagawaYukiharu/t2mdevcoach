80c046f7022c52bcc0d032d8b61670b1672d8a6f
master <master@localhost.localdomain>
2017-09-07 11:11:32 -0300

    Telas

--- src/com/t2m/devcoach/controller/Main.java
+++ src/com/t2m/devcoach/controller/Main.java
@@ -7,6 +7,7 @@ package com.t2m.devcoach.controller;
 
 import com.t2m.devcoach.model.Endereco;
 import com.t2m.devcoach.model.Pessoa;
+import com.t2m.devcoach.view.MenuFrame;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -20,29 +21,33 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
     
-        EntityManagerFactory objFactory = Persistence.createEntityManagerFactory("t2mPU");
-            EntityManager manager = objFactory.createEntityManager();
-            PessoaJpaController jpa = new PessoaJpaController(objFactory);
-            EnderecoJpaController ejpa = new EnderecoJpaController(objFactory);
-            
-            Endereco endereco = new Endereco();
-            endereco.setRua("rua");
-            endereco.setBairro("bairro");
-            endereco.setNumero("22");
-            endereco.setCidade("Campinas");
-            endereco.setEstado("SP");
-            endereco.setPais("Brasil");
-            
-            ejpa.create(endereco);
-            
-            Pessoa pessoa = new Pessoa();
-            pessoa.setNome("Nome");
-            pessoa.setDocumento("asd");
-            pessoa.setDatanasc("1999-09-09");
-            pessoa.setEmail("mas@asd.com");
-            pessoa.setIdEndereco(endereco);
-            
-            jpa.create(pessoa);
+//        EntityManagerFactory objFactory = Persistence.createEntityManagerFactory("t2mPU");
+//            EntityManager manager = objFactory.createEntityManager();
+//            PessoaJpaController jpa = new PessoaJpaController(objFactory);
+//            EnderecoJpaController ejpa = new EnderecoJpaController(objFactory);
+//            
+//            Endereco endereco = new Endereco();
+//            endereco.setRua("rua");
+//            endereco.setBairro("bairro");
+//            endereco.setNumero("22");
+//            endereco.setCidade("Campinas");
+//            endereco.setEstado("SP");
+//            endereco.setPais("Brasil");
+//            
+//            ejpa.create(endereco);
+//            
+//            Pessoa pessoa = new Pessoa();
+//            pessoa.setNome("Nome");
+//            pessoa.setDocumento("asd");
+//            pessoa.setDatanasc("1999-09-09");
+//            pessoa.setEmail("mas@asd.com");
+//            pessoa.setIdEndereco(endereco);
+//            
+//            jpa.create(pessoa);
         
+        MenuFrame menuframe = new MenuFrame();
+        menuframe.setLocationRelativeTo(null);
+        menuframe.setVisible(true);
+
     }
 }
